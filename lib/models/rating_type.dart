bf4d0827f7d982e76d5a0c6bed69f6f071a9640f
Marcel Gruessinger <marcel.gruessinger@gmail.com>
2021-02-23 22:55:42 +0100

    #13 | MODIFIED: Unified models for rating types

--- lib/models/rating_type.dart
+++ lib/models/rating_type.dart
@@ -1,20 +1,34 @@
 import 'package:goateam/models/rating_type_attribute.dart';
+import 'package:goateam/utils/database/constants/database_constants.dart';
 
 class RatingType {
+  int _id;
   String _name;
   List<RatingTypeAttribute> _attributes;
 
   RatingType(this._name, this._attributes);
 
-  String get name => _name;
-
-  List<RatingTypeAttribute> get attributes => _attributes;
+  int get id => _id;
 
   set name(String name) {
     this._name = name;
   }
 
+  String get name => _name;
+
   set attributes(List<RatingTypeAttribute> attributes) {
-    this._attributes.addAll(attributes);
+    this._attributes = attributes;
+  }
+
+  List<RatingTypeAttribute> get attributes => _attributes;
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map[DatabaseConstants.RTY_C_RATING_TYPE_ID] = _id;
+    }
+    map[DatabaseConstants.RTY_C_RATING_TYPE_NAME] = _name;
+
+    return map;
   }
 }
--- lib/models/rating_type_attribute.dart
+++ lib/models/rating_type_attribute.dart
@@ -1,11 +1,30 @@
+import 'package:goateam/utils/database/constants/database_constants.dart';
+
 class RatingTypeAttribute {
+  int _id;
+  int _ratingTypeId;
   String _name;
 
   RatingTypeAttribute(this._name);
+
+  int get id => _id;
+
+  int get ratingTypeId => _ratingTypeId;
 
   String get name => _name;
 
   set name(String name) {
     this._name = name;
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map[DatabaseConstants.RTA_C_RATING_TYPE_ATTRIBUTES_ID] = _id;
+    }
+    map[DatabaseConstants.RTA_C_RATING_TYPE_ID] = _ratingTypeId;
+    map[DatabaseConstants.RTA_C_ATTRIBUTE_NAME] = _name;
+
+    return map;
   }
 }
