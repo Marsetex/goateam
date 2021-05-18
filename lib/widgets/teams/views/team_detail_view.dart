3be8d4bc79c25572f54ba38cb487cb9b9d8193bc
Marcel Gruessinger <marcel.gruessinger@gmail.com>
2021-05-19 01:34:15 +0200

    #8 | MOD: Using of new PlayerEditorView

--- lib/widgets/teams/views/team_detail_view.dart
+++ lib/widgets/teams/views/team_detail_view.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:goateam/models/team.dart';
+import 'package:goateam/widgets/players/views/player_editor_view.dart';
 import 'package:goateam/widgets/teams/details/team_detail_body.dart';
 
 class TeamDetailView extends StatefulWidget {
@@ -16,7 +19,21 @@ class _TeamDetailViewState extends State<TeamDetailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          Route route = MaterialPageRoute(
+              builder: (context) => PlayerEditorView(widget._team, null));
+          Navigator.push(context, route).then(_onGoBack);
+        },
+      ),
       body: TeamDetailBody(widget._team),
     );
+  }
+
+  FutureOr _onGoBack(dynamic value) {
+    setState(() {
+      //_teams = getTeams();
+    });
   }
 }
