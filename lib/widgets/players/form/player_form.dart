bdb391ab0b921d643c500b10ae1dd7de7065d5f6
Marcel Gruessinger <marcel.gruessinger@gmail.com>
2021-05-19 01:33:06 +0200

    #8 | ADD: Implemented first version of form to add/edit players

---
+++ lib/widgets/players/form/player_form.dart
@@ -0,0 +1,90 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+
+import 'package:goateam/models/player.dart';
+import 'package:goateam/models/team.dart';
+import 'package:goateam/widgets/shared/image_picker/image_picker_wrapper.dart';
+
+class PlayerForm extends StatefulWidget {
+  final Team _team;
+  final Player _player;
+
+  PlayerForm(this._team, this._player);
+
+  @override
+  _PlayerFormState createState() => _PlayerFormState();
+}
+
+class _PlayerFormState extends State<PlayerForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _playerNameController = new TextEditingController();
+
+  Uint8List _rawImage;
+  //RatingType _ratingType;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget._player != null) {
+      _playerNameController.text = widget._player.name;
+      _rawImage = widget._player.profilePic;
+      // _ratingType = widget._player.ratingType;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Column(
+              children: <Widget>[
+                ImagePickerWrapper(_rawImage, _getImageFromPicker),
+                SizedBox(
+                  height: 15.0,
+                ),
+                TextFormField(
+                  controller: _playerNameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter player name',
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a name';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                //SkillRatingPicker(_ratingType, _getSkillRatingFromPicker),
+                SizedBox(
+                  height: 15.0,
+                ),
+                // Padding(
+                //   padding: const EdgeInsets.symmetric(vertical: 16.0),
+                //   child: ElevatedButton(
+                //     onPressed: () {
+                //       if (_formKey.currentState.validate()) {
+                //         _handleOnPressed();
+                //       }
+                //     },
+                //     child: Text('Submit'),
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  _getImageFromPicker(Uint8List rawImage) {
+    _rawImage = rawImage;
+  }
+}
