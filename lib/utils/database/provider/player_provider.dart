5c145e60b3461a8ab6d4a8018c1d0a725a9c7463
Marcel Gruessinger <marcel.gruessinger@gmail.com>
2021-06-13 02:09:44 +0200

    #19 | MOD: Implemented getPlayers() and insertPlayer()

--- lib/utils/database/provider/player_provider.dart
+++ lib/utils/database/provider/player_provider.dart
@@ -1,23 +1,59 @@
+import 'package:goateam/models/membership.dart';
+import 'package:goateam/models/rating.dart';
+import 'package:goateam/utils/converter/player_converter.dart';
+import 'package:sqflite/sqflite.dart';
+
 import 'package:goateam/models/player.dart';
+import 'package:goateam/models/team.dart';
+import 'package:goateam/utils/database/constants/database_constants.dart';
 import 'package:goateam/utils/database/provider/provider_base.dart';
 
 class PlayerProvider extends ProviderBase {
-  Future<List<Player>> getPlayers() async {
-    return <Player>[
-      Player('Name 1', null),
-      Player('Name 2', null),
-      Player('Name 3', null),
-      Player('Name 4', null),
-      Player('Name 5', null),
-      Player('Name 6', null),
-      Player('Name 7', null),
-      Player('Name 8', null),
-      Player('Name 9', null),
-      Player('Name 10', null),
-      Player('Name 11', null),
-      Player('Name 12', null),
-      Player('Name 13', null),
-      Player('Name 14', null),
-    ];
+  Future<List<Player>> getPlayers(Team team, [Database db]) async {
+    Database dbContext = await setDbContextIfNull(db);
+
+    List<Map<String, dynamic>> result = await dbContext.rawQuery('''
+      SELECT  ${DatabaseConstants.PLR_T_NAME}.${DatabaseConstants.PLR_C_PLAYER_ID},
+              ${DatabaseConstants.PLR_T_NAME}.${DatabaseConstants.PLR_C_PLAYER_NAME},
+              ${DatabaseConstants.PLR_T_NAME}.${DatabaseConstants.PLR_C_PROFILE_PIC}
+      FROM ${DatabaseConstants.PLR_T_NAME}
+      INNER JOIN ${DatabaseConstants.MSP_T_NAME} on 
+        ${DatabaseConstants.MSP_T_NAME}.${DatabaseConstants.MSP_C_PLAYER_ID} = ${DatabaseConstants.PLR_T_NAME}.${DatabaseConstants.PLR_C_PLAYER_ID}
+      WHERE ${DatabaseConstants.MSP_T_NAME}.${DatabaseConstants.MSP_C_TEAM_ID} = ${team.id};
+    ''');
+
+    return PlayerConverter().convert(result);
+  }
+
+  Future<bool> insertPlayer(Player player, Team team, Map<int, int> ratingsMap,
+      [Database db]) async {
+    Database dbContext = await setDbContextIfNull(db);
+
+    return await dbContext.transaction((txn) async {
+      int playerId = await txn.insert(
+          DatabaseConstants.PLR_T_NAME, player.toMap(),
+          conflictAlgorithm: ConflictAlgorithm.rollback);
+
+      if (playerId < 1) {
+        return false;
+      }
+
+      var membership = new Memebership(team.id, playerId);
+      int membershipId = await txn.insert(
+          DatabaseConstants.MSP_T_NAME, membership.toMap(),
+          conflictAlgorithm: ConflictAlgorithm.rollback);
+
+      if (membershipId < 1) {
+        return false;
+      }
+
+      ratingsMap.forEach((key, value) {
+        var playerRating = new Rating(membershipId, key, value);
+        txn.insert(DatabaseConstants.RTS_T_NAME, playerRating.toMap(),
+            conflictAlgorithm: ConflictAlgorithm.rollback);
+      });
+
+      return true;
+    });
   }
 }
